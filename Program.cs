5397526f13ca75aefc13af95a12bead657ab3abd
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 20:43:27 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,28 @@
-﻿using System;
+using System;
+using System.IO;
 
-class FlowerShop
+class Book
 {
-    public virtual string CreateFlower() => "Троянда ";
+    public required string Title { get; set; }
+    public required string Author { get; set; }
 }
 
-class TulipShop : FlowerShop
+class BookSaver
 {
-    public override string CreateFlower() => "Тюльпан ";
+    public void Save(Book book)
+    {
+        File.WriteAllText("book.txt", $"{book.Title} by {book.Author}");
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        FlowerShop roseShop = new FlowerShop();
-        FlowerShop tulipShop = new TulipShop();
-
-        Console.WriteLine(roseShop.CreateFlower());
-        Console.WriteLine(tulipShop.CreateFlower());
+        Book book = new Book { Title = "1984", Author = "George Orwell" };
+        BookSaver saver = new BookSaver();
+        saver.Save(book);
+        Console.WriteLine("Book saved!");
     }
 }
 
